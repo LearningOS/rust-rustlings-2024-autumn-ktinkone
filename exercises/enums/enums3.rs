3dc8cb1a0a32e1e1b28afa19d8f5a16dd6976596
ktinkone <ktinkone@gmail.com>
2024-10-07 19:57:25 +0800

    finish ex36-40

--- exercises/enums/enums3.rs
+++ exercises/enums/enums3.rs
@@ -5,10 +5,12 @@
 // Execute `rustlings hint enums3` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
 
 enum Message {
-    // TODO: implement the message variant types based on their usage below
+    ChangeColor(u8, u8, u8),
+    Echo(String),
+    Move(Point),
+    Quit,
 }
 
 struct Point {
@@ -39,10 +41,21 @@ impl State {
     }
 
     fn process(&mut self, message: Message) {
-        // TODO: create a match expression to process the different message
-        // variants
-        // Remember: When passing a tuple as a function argument, you'll need
-        // extra parentheses: fn function((t, u, p, l, e))
+        match message {
+            Message::ChangeColor (i, j, k) => {
+                let color = (i, j, k);
+                State::change_color(self, color);
+            },
+            Message::Echo (string) => {
+                State::echo(self, string);
+            },
+            Message::Move(point) => {
+                State::move_position(self, point);
+            },
+            Message::Quit => {
+                State::quit(self);
+            },
+        }
     }
 }
 
--- exercises/strings/strings3.rs
+++ exercises/strings/strings3.rs
@@ -3,21 +3,45 @@
 // Execute `rustlings hint strings3` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
 
 fn trim_me(input: &str) -> String {
     // TODO: Remove whitespace from both ends of a string!
-    ???
+    let s = input.as_bytes();
+    let len = input.len();
+
+    let mut start = 0;
+    for i in 0..len {
+        if s[i] != b' ' {
+            start = i;
+            break;
+        }
+    };
+
+    let mut end = len - 1;
+    for i in (0..len).rev() {
+        if s[i] != b' ' {
+            end = i;
+            break;
+        }
+    };
+
+    let str = String::from(&input[start..=end]);
+
+    str
 }
 
 fn compose_me(input: &str) -> String {
     // TODO: Add " world!" to the string! There's multiple ways to do this!
-    ???
+    let mut s = String::from(input);
+    s.insert_str(s.len(), " world!");
+    s
 }
 
 fn replace_me(input: &str) -> String {
     // TODO: Replace "cars" in the string with "balloons"!
-    ???
+    let mut s1 = String::from(input);
+    let s2 = s1.replace("cars", "balloons");
+    s2
 }
 
 #[cfg(test)]
